30bdb794003a3b38655f656890b99e601aa23aaa
Wolfy Fiorini <jonwolffiorini@gmail.com>
2023-04-23 15:07:25 -0600

    cors

--- src/helpers/RecipeService.js
+++ src/helpers/RecipeService.js
@@ -2,7 +2,7 @@ const api = process.env.REACT_APP_API_KEY
 
 export async function getRecipes(url) {
 	let recipes = []
-	await fetch(api + "/recipes?" + url, {mode: 'cors'})
+	await fetch(api + "recipes?" + url, {mode: 'cors'})
 	.then((res) => {
 	  recipes = res.json()
 	})
@@ -10,20 +10,20 @@ export async function getRecipes(url) {
 }
 
 export async function getCreatedRecipes() {
-	let created = await fetch(api + "/recipes/created", {mode: 'cors'})
+	let created = await fetch(api + "recipes/created", {mode: 'cors'})
 	created = await created.json()
 	return created
 }
 
 export async function getFavoritedRecipes() {
-	let favorites = await fetch(api + "/recipes/favorited", {mode: 'cors'})
+	let favorites = await fetch(api + "recipes/favorited", {mode: 'cors'})
 	favorites = await favorites.json()
 	return favorites
 }
 
 export async function getOneRecipe(id) {
 	let recipe = null
-	await fetch("api + /recipes/"+id, {mode: 'cors'})
+	await fetch(api + "recipes/"+id, {mode: 'cors'})
 	.then((res) => {
 	  recipe = res.json()
 	})
@@ -32,7 +32,7 @@ export async function getOneRecipe(id) {
 
 export async function addRecipe(data){
 	console.log(data)
-	return await fetch(api + "/recipes/create", {
+	return await fetch(api + "recipes/create", {
 		method: "POST", 
 		mode: 'cors',
 		headers: {
@@ -46,7 +46,7 @@ export async function uploadImage(form){
 	if(!form) {
 		return 
 	}
-	return await fetch(api + "/recipes/upload", {
+	return await fetch(api + "recipes/upload", {
 		method: "POST",
 		mode: 'cors',
 		body: form
@@ -55,7 +55,7 @@ export async function uploadImage(form){
 
 export async function editRecipe(data) {
 	console.log(data)
-	await fetch(api + "/recipes/"+data._id, {
+	await fetch(api + "recipes/"+data._id, {
 		method: "PATCH",
 		mode: 'cors',
 		headers: {
@@ -66,7 +66,7 @@ export async function editRecipe(data) {
 }
 
 export async function deleteRecipe(id) {
-	await fetch(api + "/recipes/"+id, {
+	await fetch(api + "recipes/"+id, {
 		method: "DELETE",
 		mode: 'cors'
 	}).catch((err) => {console.log(err)})
