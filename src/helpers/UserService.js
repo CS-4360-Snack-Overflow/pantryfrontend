1da25712080982f04111eb3bb9fe4c30da5413da
Wolfy319 <jonwolffiorini@gmail.com>
2023-04-24 11:01:46 -0600

    errors

--- src/helpers/UserService.js
+++ src/helpers/UserService.js
@@ -1,14 +1,14 @@
 const api = process.env.REACT_APP_API_KEY
 
 export async function checkAuth(recipe_user_id){
-    let data = await fetch(api + "user-id", {mode: 'cors'})
+    let data = await fetch(api + "user-id", {mode: 'cors'}).catch((err) => console.log(err))
     data = await data.json()
     console.log(data)
     return data.userId === recipe_user_id
 }
 
 export async function checkFavorited(recipe_id){
-    let data = await fetch(api + "user/isfavorite/" + recipe_id, {mode: 'cors'})
+    let data = await fetch(api + "user/isfavorite/" + recipe_id, {mode: 'cors'}).catch((err) => console.log(err))
     data = await data.json()
     console.log(data)
     return data.result
@@ -25,7 +25,7 @@ export async function removeFavoriteRecipe(recipe_id){
 }
 
 export async function getUser(){
-    const res = await fetch(api + "user/userRead", {mode: 'cors'})
+    const res = await fetch(api + "user/userRead", {mode: 'cors'}).catch((err) => console.log(err))
     return await res.json()
 }
 
