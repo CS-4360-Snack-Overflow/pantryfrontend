89584ba3a27724b0f4c6fae8bda5a47e759c938c
Wolfy319 <jonwolffiorini@gmail.com>
2023-04-24 12:32:07 -0600

    send cookie credentials

--- src/helpers/UserService.js
+++ src/helpers/UserService.js
@@ -1,7 +1,7 @@
 const api = process.env.REACT_APP_API_KEY
 
 export async function checkRecipeAuth(recipe_user_id){
-    let data = await fetch(api + "user-id", {mode: 'cors'}).catch((err) => console.log(err))
+    let data = await fetch(api + "user-id", {mode: 'cors', credentials : "include"}).catch((err) => console.log(err))
     data = await data.json()
     console.log(data)
     return data.userId === recipe_user_id
@@ -19,24 +19,24 @@ export async function testUserAuth() {
 
 
 export async function checkFavorited(recipe_id){
-    let data = await fetch(api + "user/isfavorite/" + recipe_id, {mode: 'cors'}).catch((err) => console.log(err))
+    let data = await fetch(api + "user/isfavorite/" + recipe_id, {mode: 'cors', credentials : "include"}).catch((err) => console.log(err))
     data = await data.json()
     console.log(data)
     return data.result
 }
 
 export async function addFavoriteRecipe(recipe_id){
-    await fetch(api + "user/favorite/" + recipe_id, {method: "POST", mode: 'cors'})
+    await fetch(api + "user/favorite/" + recipe_id, {method: "POST", mode: 'cors', credentials : "include",})
     return
 }
 
 export async function removeFavoriteRecipe(recipe_id){
-    await fetch(api + "user/unfavorite/" + recipe_id, {method: "DELETE", mode: 'cors'})
+    await fetch(api + "user/unfavorite/" + recipe_id, {method: "DELETE", mode: 'cors', credentials : "include",})
     return
 }
 
 export async function getUser(){
-    let res = await fetch(api + "user/userRead", {mode: 'cors'}).catch((err) => console.log(err))
+    let res = await fetch(api + "user/userRead", {mode: 'cors', credentials : "include",}).catch((err) => console.log(err))
     res = await res.json()
     console.log(res)
     return res
@@ -47,6 +47,7 @@ export async function login(username, password){
     let response = await fetch(api + 'user/userLoginProc', {
         method: "POST", 
         mode: 'cors',
+        credentials : "include",
         headers: {
           "Content-Type": "application/json",
           },
